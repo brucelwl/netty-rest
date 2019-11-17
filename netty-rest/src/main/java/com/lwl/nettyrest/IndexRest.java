00c5eb0542759e16aa2487690e50e58f3da85f7d
lwl <liwenlong@zto.com>
2019-11-17 18:18:41 +0800

    添加http Keep-Alive 和 gzip压缩支持

--- netty-rest/src/main/java/com/lwl/nettyrest/IndexRest.java
+++ netty-rest/src/main/java/com/lwl/nettyrest/IndexRest.java
@@ -15,28 +15,36 @@ import java.util.ArrayList;
 @Rest("index")
 public class IndexRest {
 
-    @ReqMapping(value = {"say","hehe"})
-    public UserInfo sayHello(@ReqParam(value = "name1",required = false) ArrayList<String> name, String address, Integer age){
+    @ReqMapping(value = {"say", "hehe"})
+    public UserInfo sayHello(@ReqParam(value = "name1", required = false) ArrayList<String> name, String address, Integer age) {
         UserInfo userInfo = new UserInfo();
         userInfo.setNickname(JSON.toJSONString(name));
         userInfo.setId(1001);
         userInfo.setLoginname("啊哈哈哈");
         userInfo.setPwd("wdwsfddsf23546+5");
+
+        //测试返回数据gzip压缩
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 357; i++) {
+            stringBuilder.append("" + i);
+        }
+        userInfo.setPwd(stringBuilder.toString());
+
         return userInfo;
     }
 
-    @ReqMapping(value = "say2",method = {ReqMethod.GET})
-    public String sayHello2(@ReqParam(required = false) String address,Integer age){
-        return address +" age "+age;
+    @ReqMapping(value = "say2", method = {ReqMethod.GET})
+    public String sayHello2(@ReqParam(required = false) String address, Integer age) {
+        return address + " age " + age;
     }
 
     @ReqMapping(value = "say3")
-    public String sayHello3(@ReqParam(required = false) String id){
-        return "哈哈哈:"+id;
+    public String sayHello3(@ReqParam(required = false) String id) {
+        return "哈哈哈:" + id;
     }
 
-    @ReqMapping(value = "say4",method = ReqMethod.POST)
-    public String sayHello4(String id){
+    @ReqMapping(value = "say4", method = ReqMethod.POST)
+    public String sayHello4(String id) {
         return id;
     }
 
